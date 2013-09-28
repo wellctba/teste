40efd8c6d84b24936e526f24a28d74eb1a4df3a1
wellctba <wellinton@aedsol.com>
2013-09-28 20:39:47 -0300

    Update teste.cs

--- teste.cs
+++ teste.cs
@@ -4,13 +4,15 @@ namespace mx5server
 {
 	class MainClass
 	{
+		int teste;
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Maximus Server - v5.0");
 			Console.WriteLine ("By Eduardo Fonseca <ebf@aedsol.com>\n");
 
 			Servidor	srv = new Servidor(8899); // inicializa um objeto servidor passando como parâmetro a porta 8899
-			srv.inicializaServidor();			  // executa o método inicializaServidor() do objeto srv
+			srv.inicializaServidor();
+			// executa o método inicializaServidor() do objeto srv
 
 		}
 	}
